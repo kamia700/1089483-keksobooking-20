3500f5cf1d88d06e8f537f69c8b15e0bb300d59e
kamia700 <kaana757@gmail.com>
2020-06-09 13:16:18 +0200

    Добавляет карточку объявления

--- js/main.js
+++ js/main.js
@@ -41,6 +41,23 @@ var getRandomElement = function (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
+var getRandomFeatures = function (features) {
+  var rendomFeatureNumbers = getRandomIntInclusive(1, FEATURES.length);
+  var lengthOfArray = features.length;
+
+  // While there remain elements to shuffle…
+  while (lengthOfArray) {
+    // Pick a remaining element…
+    var i = Math.floor(Math.random() * lengthOfArray--);
+    // And swap it with the current element.
+    var t = features[lengthOfArray];
+    features[lengthOfArray] = features[i];
+    features[i] = t;
+  }
+  return features.slice(0, rendomFeatureNumbers);
+};
+// getRandomFeatures(FEATURES);
+
 var createAds = function (count) {
   var locationX = getRandomIntInclusive(0, pinMapWidth);
   var locationY = getRandomIntInclusive(Y_TOP, Y_BOTTOM);
@@ -57,7 +74,7 @@ var createAds = function (count) {
       guests: getRandomElement(GUESTS),
       checkin: getRandomElement(CHECKIN),
       checkout: getRandomElement(CHECKOUT),
-      features: getRandomElement(FEATURES),
+      features: getRandomFeatures(FEATURES),
       description: 'строка с описанием',
       photos: PHOTOS},
     location: {
@@ -93,7 +110,10 @@ var renderPin = function (ads) {
 var addPins = function (offers) {
   var fragment = document.createDocumentFragment();
   for (var j = 0; j < offers.length; j++) {
-    fragment.appendChild(renderPin(offers[j]));
+    // проверка для п.5.2 ТЗ
+    if (offers[j].offer) {
+      fragment.appendChild(renderPin(offers[j]))
+    }
   }
   pinMap.appendChild(fragment);
 };
@@ -102,3 +122,98 @@ var offers = getOffers();
 
 document.querySelector('.map').classList.remove('map--faded');
 addPins(offers);
+
+// дз 2
+var cardTemplate = document.querySelector('#card')
+.content
+.querySelector('.map__card');
+
+var housingTypes = {
+  palace: 'Дворец',
+  flat: 'Квартира',
+  house: 'Дом',
+  bungalo: 'Бунгало',
+};
+
+var WIDTH_PHOTO_CARD = 45;
+var HEIGHT_PHOTO_CARD = 40;
+var PHOTO_ALT_CARD = 'Фотография жилья';
+
+// Вставьте полученный DOM-элемент в блок .map перед блоком .map__filters-container
+var map = document.querySelector('.map');
+var filtersContainer = map.querySelector('.map__filters-container');
+
+var renderPhotos = function (container, photos) {
+  container.innerHTML = '';
+
+  for (var i = 0; i < photos.length; i++) {
+    // debugger;
+    var photoElement = document.createElement('img');
+    photoElement.src = photos[i];
+    photoElement.classList.add('popup__photo');
+    photoElement.width = WIDTH_PHOTO_CARD;
+    photoElement.height = HEIGHT_PHOTO_CARD;
+    photoElement.alt = PHOTO_ALT_CARD;
+
+    container.appendChild(photoElement);
+  }
+};
+// или так
+// var renderPhotos = function (photos) {
+//   var imgPhoto = '';
+
+//   for (var i = 0; i < photos.length; i++) {
+//     imgPhoto += '<img src="' + photos[i] + '" class="popup__photo" width="45" height="40" alt="Фотография жилья">';
+//   }
+//   return imgPhoto;
+// };
+
+// тут функция отрабатывает через раз. Не найти причину
+// выдает ошибку InvalidCharacterError: String contains an invalid character main.js:225
+
+var renderFeatures = function (container, features) {
+  container.innerHTML = '';
+
+  for (var i = 0; i < features.length; i++) {
+    // debugger;
+    var featureElement = document.createElement('li');
+
+    featureElement.classList.add('popup__feature');
+    featureElement.classList.add('popup__feature--' + features[i]);
+
+    container.appendChild(featureElement);
+  }
+};
+
+
+// генерирую DOM-элементы
+var renderCard = function (ads) {
+  var cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector('.popup__title').textContent = ads.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = ads.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = ads.offer.price + '₽/ночь';
+  cardElement.querySelector('.popup__type').textContent = housingTypes[ads.offer.type];
+  cardElement.querySelector('.popup__text--capacity').textContent = ads.offer.rooms + ' комнаты' + ' для ' + ads.offer.guests + ' гостей';
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + ads.offer.checkin + ', выезд до ' + ads.offer.checkout;
+  cardElement.querySelector('.popup__description').textContent = ads.offer.description;
+  // cardElement.querySelector('.popup__photos').innerHTML = renderPhotos(ads.offer.photos);
+
+
+  var cardFeatures = cardElement.querySelector('.popup__features');
+  cardFeatures.innerHTML = '';
+  // если выводжу текст, массива генерится в случайном порядке и отображается.
+  // с иконками работает через раз
+  // cardFeatures.textContent = ads.offer.features;
+  renderFeatures(cardFeatures, ads.offer.features);
+
+  var cardPhotos = cardElement.querySelector('.popup__photos');
+  renderPhotos(cardPhotos, ads.offer.photos);
+
+  cardElement.querySelector('.popup__avatar').src = ads.author.avatar;
+
+  return cardElement;
+};
+
+map.insertBefore(renderCard(offers[0]), filtersContainer);
+
